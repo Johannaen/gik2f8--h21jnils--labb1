8993f4b9a1dfa410df6b21428ecb95ea37554ab4
Johanna Engdahl Nilsson <johanna.en@hotmail.com>
2022-12-07 14:48:06 +0100

    Finlir

--- Components/BookCard.js
+++ Components/BookCard.js
@@ -2,14 +2,14 @@
 function getBookCard(book) {
 
     let html = ` 
-    <div id="bookDetails" style="position:absolute;width:20vw" >
+    <div id="bookDetails" style="position:absolute;width:20vw" style="position:absolute;height:20vw" class="bg-indigo-100 border-4 rounded-lg grid justify-items-center"  >
         <div class="px-6 py-4">
             <div class="font-bold text-xl mb-2">${book.title}</div>
                 <p class="text-gray-700 text-base">
                     Information om boken du sökt på: 
                 </p>
             </div>
-        <img class="w-full" src="${book.coverImage}" alt="${book.title}coverImage">
+        <img class="max-h-48"  src="${book.coverImage}" alt="${book.title}coverImage">
     </div>`;
 
     return html;
--- Components/BookListItem.js
+++ Components/BookListItem.js
@@ -1,6 +1,6 @@
 const BookListItem = (book) => {
-  let html = `<li onmouseover="showDetails(event)" onmouseout="removeDetails(event)"
-                id="${book.id}" class="book-list__item mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b border-indigo-700 cursor-pointer">
+  let html = `<li onmouseenter="showDetails(event)" onmouseout="removeDetails(event)"
+                id="${book.id}" class="book-list__item mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b border-gray-700 cursor-pointer">
               ${book.author} - ${book.title}    
               </li>`;
   return html;
