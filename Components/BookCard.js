5405d67c353cb127cf2526098c5bb601267921e8
Johanna Engdahl Nilsson <johanna.en@hotmail.com>
2022-12-09 13:56:11 +0100

    Sista commit

--- Components/BookCard.js
+++ Components/BookCard.js
@@ -2,11 +2,14 @@
 function getBookCard(book) {
 
     let html = ` 
-    <div id="bookDetails" style="position:absolute;width:20vw" style="position:absolute;height:20vw" class="bg-indigo-100 border-4 rounded-lg grid justify-items-center"  >
-        <div class="px-6 py-4">
+    <div id="bookDetails" style="position:absolute;width:18rem" style="position:absolute;height:30vw" class="bg-indigo-100 border-4 rounded-lg grid justify-items-center"  >
+        <div class="px-6 py-4 p-3">
             <div class="font-bold text-xl mb-2">${book.title}</div>
-                <p class="text-gray-700 text-base">
-                    Information om boken du sökt på: 
+            <p class="text-gray-700 text-base">
+                    Författare: ${book.author}
+                </p>    
+            <p class="text-gray-700 text-base">
+                    Utgivningsår: ${book.releaseDate}
                 </p>
             </div>
         <img class="max-h-48"  src="${book.coverImage}" alt="${book.title}coverImage">
