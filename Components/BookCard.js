ba519cc185ef0ad47a4ec4b885218eb3b85eaf53
Johanna Engdahl Nilsson <johanna.en@hotmail.com>
2022-12-09 14:18:48 +0100

    Absolut sista commit

--- Components/BookCard.js
+++ Components/BookCard.js
@@ -2,7 +2,7 @@
 function getBookCard(book) {
 
     let html = ` 
-    <div id="bookDetails" style="position:absolute;width:18rem" style="position:absolute;height:30vw" class="bg-indigo-100 border-4 rounded-lg grid justify-items-center"  >
+    <div id="bookDetails" style="position:absolute;width:18rem" style="position:absolute;height:30vw" class="bg-indigo-200 border-4 rounded-lg grid justify-items-center"  >
         <div class="px-6 py-4 p-3">
             <div class="font-bold text-xl mb-2">${book.title}</div>
             <p class="text-gray-700 text-base">
